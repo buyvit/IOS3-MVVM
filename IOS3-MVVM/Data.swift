764a93f6372f70d46e8f3073dbf70ec29dfa3021
Vitaly Buylushkin <vitalybuylushkin@yandex.ru>
2021-11-05 21:47:03 +0300

    MVC architecture
    создали проект в архитектуре MVC

---
+++ IOS3-MVVM/Data.swift
@@ -0,0 +1,10 @@
+//
+//  Data.swift
+//  IOS3-MVVM
+//
+//  Created by Vitaly Buylushkin on 05.11.2021.
+//
+
+import Foundation
+
+ var data = ["Авиарежим", "Wi-Fi", "Bluetooth", "Сотовая связь","Режим модема" , "VPN", "Уведомления", "Звуки, тактильные сигналы", "Фокусирование", "Экранное время"]
